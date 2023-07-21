f89d78c22d3931c72399ef31f9d6e6c032d2efdd
shweta <junnarkarshweta3@gmail.com>
2023-07-21 18:09:11 +0530

    Swap Value using third Variable.

---
+++ SwapVariableValue.java
@@ -0,0 +1,13 @@
+public class SwapVariableValue {
+
+    public static void main(String[] args)
+    {
+        int i=10;
+        int j=20;
+        int c;
+        c=i;
+        i=j;
+        j=c;
+        System.out.println("i= "+i+" "+"j="+j);
+    }
+}
