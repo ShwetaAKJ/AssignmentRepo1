e988d65417980c3a24fb655cf312407399c86267
shweta <junnarkarshweta3@gmail.com>
2023-07-21 17:48:18 +0530

    Swap Values of Variable without using third variable.

---
+++ VariableExcersice.java
@@ -0,0 +1,14 @@
+public class VariableExcersice {
+    public static void main(String[] args)
+    {
+        int i= 10;
+        int j= 7;
+        System.out.println(i);
+        System.out.println(j);
+        i=i+j;
+        j=i-j;
+        i=i-j;
+        System.out.println("i="+i+" "+"j="+j);
+
+    }
+}
