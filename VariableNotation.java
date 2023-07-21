fe4d0db660739b54ea81cbd73800b570adce5f1c
shweta <junnarkarshweta3@gmail.com>
2023-07-21 18:03:05 +0530

    Variable Notation

---
+++ VariableNotation.java
@@ -0,0 +1,18 @@
+public class VariableNotation {
+    public static void main(String[] args)
+    {
+        int i = 10; // correct notation
+
+
+       // int int = 20;  showing error
+
+       // int goto = 30;
+
+        //int default = 40;
+
+       // int class = 50;
+
+        int j = 70; //correct notation
+
+    }
+}
