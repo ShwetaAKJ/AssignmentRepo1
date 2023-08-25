5f3f0029794ac6c7e638a9a091928c33c16ccf3f
shweta <junnarkarshweta3@gmail.com>
2023-08-25 13:55:46 +0530

    Infinite while loop

---
+++ InfiniteWhileLoop.java
@@ -0,0 +1,32 @@
+public class InfiniteWhileLoop {
+    public static void main(String[] args) {
+       // int i=0;
+        //increment is absent here....
+      /*  while(i<=10)
+        {
+            System.out.println(i);
+
+        }*/
+
+        //continue....
+       /* while(i<=10)
+        {
+            System.out.println(i);
+            if(i==10)
+            {
+
+               continue;
+            }
+            i++;
+
+        }*/
+
+        //condition not specified.....
+       /* while(true)
+        {
+            System.out.println(i);
+            i++;
+        }*/
+
+    }
+}
