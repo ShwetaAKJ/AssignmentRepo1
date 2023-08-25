4615220d78ceebb631ca715bf00ba45a81c19279
shweta <junnarkarshweta3@gmail.com>
2023-08-25 14:44:35 +0530

    Infinite for loop....

---
+++ InfiniteForLoop.java
@@ -0,0 +1,17 @@
+public class InfiniteForLoop {
+    public static void main(String[] args) {
+        //condition is not present..
+       /* for(int i=0;;i++)
+        {
+            System.out.println(i);
+        }*/
+
+        //continue....
+      /*  for(int i=0;i<=5;)
+        {
+            System.out.println(i);
+           if(i==5)continue;
+            i++;
+        }*/
+    }
+}
